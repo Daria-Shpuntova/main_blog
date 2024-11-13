99ab621067055f41d673248652b48ec66723ed15
Дарья <133221331+Daria-Shpuntova@users.noreply.github.com>
2024-11-13 17:41:06 +0300

    Update settings.py

--- HelloDjango/HelloDjango/settings.py
+++ HelloDjango/HelloDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)pk09idn5(ma^yb%mq23kzfg2c35h2_99g)k4!^habdfw$+gvq'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,7 +148,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 2525
 EMAIL_HOST_USER = 'shpuntova.darya@mail.ru'
-EMAIL_HOST_PASSWORD = 'd54215Mh2YptY3qMNisd'
+EMAIL_HOST_PASSWORD = '#'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
